19db6b79e1878587fcbca42afcbff689fef32bf7
RosanaGarciap <97164329+RosanaGarciap@users.noreply.github.com>
2022-04-03 00:43:01 -0400

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,8 +1,5 @@
-import os
-import random
-
 from game.casting.actor import Actor
-from game.casting.artifact import Artifact
+from game.casting.aliens import Aliens
 from game.casting.cast import Cast
 
 from game.directing.director import Director
@@ -10,22 +7,8 @@ from game.directing.director import Director
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
 
-from game.shared.color import Color
+from game.shared.constants import *
 from game.shared.point import Point
-
-
-FRAME_RATE = 12
-MAX_X = 900
-MAX_Y = 600
-CELL_SIZE = 15
-FONT_SIZE = 15
-COLS = 60
-ROWS = 40
-CAPTION = "Robot Finds Kitten"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
-WHITE = Color(255, 255, 255)
-DEFAULT_ARTIFACTS = 40
-
 
 def main():
     
@@ -42,43 +25,19 @@ def main():
     
     # create the robot
     x = int(MAX_X / 2)
-    y = int(MAX_Y / 2)
+    y = int(585)
     position = Point(x, y)
 
     robot = Actor()
-    robot.set_text("#")
+    robot.set_text("W")
     robot.set_font_size(FONT_SIZE)
     robot.set_color(WHITE)
     robot.set_position(position)
-    cast.add_actor("robots", robot)
-    
-    # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    cast.add_actor("heros", robot)
 
-    for n in range(DEFAULT_ARTIFACTS):
-        text = chr(random.randint(33, 126))
-        message = messages[n]
-
-        x = random.randint(1, COLS - 1)
-        y = random.randint(1, ROWS - 1)
-        position = Point(x, y)
-        position = position.scale(CELL_SIZE)
-
-        r = random.randint(0, 255)
-        g = random.randint(0, 255)
-        b = random.randint(0, 255)
-        color = Color(r, g, b)
-        
-        artifact = Artifact()
-        artifact.set_text(text)
-        artifact.set_font_size(FONT_SIZE)
-        artifact.set_color(color)
-        artifact.set_position(position)
-        artifact.set_message(message)
-        cast.add_actor("artifacts", artifact)
-    
+    for n in range(DEFAULT_ARTIFACTS):        
+        artifact = Aliens()
+        cast.add_actor("alien", artifact)
     # start the game
     keyboard_service = KeyboardService(CELL_SIZE)
     video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
